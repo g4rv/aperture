d969988c3b2228fe7f0138604743fa18d9e3ff81
g4rv <skydorw@gmail.com>
2022-06-09 01:45:28 +0300

    aperture js error fixed

--- #src/assets/js/modules/headerFindHeight.js
+++ #src/assets/js/modules/headerFindHeight.js
@@ -2,7 +2,6 @@ const headerFindHeight = () => {
     const headerHeight = document.querySelector('.header').offsetHeight;
 
     document.documentElement.style.setProperty('--header-h', headerHeight + 'px');
-    console.log(headerHeight)
 }
 
 export default headerFindHeight;
